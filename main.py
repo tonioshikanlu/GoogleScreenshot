3afa1f8835cc05e930377b2e8d5139966f9fb441
Oluwatoni Oshikanlu <39501338+tonioshikanlu@users.noreply.github.com>
2018-10-16 23:30:51 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ from pytesseract import image_to_string
 from googlesearch import search
 import webbrowser
 
-def Url_Opener():
+def Url_Opener(imagefile):
     Url_list=[]
-    image_text = (image_to_string(Image.open('download.jpeg')))
+    image_text = (image_to_string(Image.open(imagefile)))
     for j in search(image_text, num=5, stop=1, pause=2):
         Url_list.append(j)
     print(Url_list)
